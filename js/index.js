843623f77d58f4e2ba23e556e12f2077f2d7244e
DanielMartinez <fantonmartinez13@gmail.com>
2023-05-24 15:42:20 -0600

    Se agrega la funcion desencriptar

--- js/index.js
+++ js/index.js
@@ -19,3 +19,23 @@ function encriptar(){
         alert("Ingrese un texto");
     }
 }
+function desEncriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("imagen-texto");
+    let textoCorrecto = texto.replace(/enter/ig, "e").replace(/imes/ig, "i").replace(/ai/ig, "a").replace(/ober/ig, "o").replace(/ufat/ig, "u");
+    //computos
+    if(texto.length != 0){
+        document.getElementById("texto").value = textoCorrecto;
+        tituloMensaje.textContent = "Texto desencriptado con exito :)";
+        parrafo.textContent = "Tu texto se encuentra donde lo esrcibiste";
+        imagen.src = "./imagenes/desencriptado.png";
+    }
+    else{
+        imagen.src = "./imagenes/searchengin.svg";
+        tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Ingrese un texto");
+    }
+}
